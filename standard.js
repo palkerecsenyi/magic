842520319a01e63114a132b890ace07d6d3c6f80
Pal Kerecsenyi <palkerecs@gmail.com>
2018-01-14 17:01:05 +0000

    Loads of stuff
    Completely re-did navbar, made colours universaller, added loads of colours, major tweaks to buttons, major tweaks to type, new font, added new font, more!

--- standard.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.btn, .project').click(function(){
-		var attr = this.getAttribute("data-link");
-		if (typeof attr !== typeof undefined && attr !== false) {
-			window.location.href = attr;
-		}
-	});
-});
