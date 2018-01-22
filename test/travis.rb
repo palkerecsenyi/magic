a23da74a8478c365af3f71989d241abef00ddff4
Pal Kerecsenyi <palkerecs@gmail.com>
2018-01-22 18:50:48 +0000

    Update travis.rb

--- test/travis.rb
+++ test/travis.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'coveralls'
+Coveralls.wear!
 result = `sass magic.scss magic.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('magic.css')
