e8a93fd325db62ace275ffd1b36a7d4f73b4c8ff
Pal Kerecsenyi <palkerecs@gmail.com>
2018-02-14 18:56:02 +0000

    Update travis.rb

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass magic.scss magic.css`
+result = `sass src/magic.scss magic.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('magic.css')
 puts "Regular compile worked successfully"
